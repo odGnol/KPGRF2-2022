1f9d8e455270765b27a0f5df0a9dcb7fe6d254b4
Long Do <long.do@emai.cz>
2022-04-03 17:08:19 +0200

    úprava Renderer3D
    - doplnění komentářů a proměnných, přejmenování vybraných metod
    - doplnění podmínky pro fci pripravTrojuhelnik (případ, kdy jsou vidět dva vrcholy trojúhelníku),podmínky pro osu y v souvislosti s ořezáním trojúhelníku

--- src/renderer/Renderer3D.java
+++ src/renderer/Renderer3D.java
@@ -3,7 +3,7 @@ package renderer;
 import model.Cast;
 import model.TypGeometrickeTopologie;
 import model.Vrchol;
-import rasterize.HloubkaBuffer;
+import rasterize.DepthBuffer;
 import rasterize.Raster;
 import transforms.*;
 
@@ -13,13 +13,13 @@ import java.util.Optional;
 public class Renderer3D implements GPURenderer {
 
     private final Raster<Integer> imageBuffer;
-    private final HloubkaBuffer depthBuffer;
+    private final DepthBuffer depthBuffer;
 
     private Mat4 model, view, projection;
 
     public Renderer3D(Raster<Integer> raster) {
         this.imageBuffer = raster;
-        depthBuffer = new HloubkaBuffer(raster.getWidth(), raster.getHeight());
+        depthBuffer = new DepthBuffer(raster.getWidth(), raster.getHeight());
 
         model = new Mat4Identity();
         view = new Mat4Identity();
@@ -27,11 +27,11 @@ public class Renderer3D implements GPURenderer {
     }
 
     @Override
-    public void nakresli(List<Cast> parts, List<Integer> ib, List<Vrchol> vb) {
-        for (Cast part : parts) {
-            TypGeometrickeTopologie topologyType = part.getGeometrickyTyp();
-            int index = part.getIndex();
-            int pocet = part.getPocet();
+    public void nakresli(List<Cast> casti, List<Integer> ib, List<Vrchol> vb) {
+        for (Cast cast : casti) {
+            TypGeometrickeTopologie topologyType = cast.getGeometrickyTyp();
+            int index = cast.getIndex();
+            int pocet = cast.getPocet();
             if (topologyType == TypGeometrickeTopologie.TROJUHELNIK) {
                 for (int i = index; i < pocet * 3 + index; i += 3) {
                     Integer i1 = ib.get(i);
@@ -41,7 +41,7 @@ public class Renderer3D implements GPURenderer {
                     Vrchol v1 = vb.get(i1);
                     Vrchol v2 = vb.get(i2);
                     Vrchol v3 = vb.get(i3);
-                    prepareTriangle(v1, v2, v3);
+                    pripravTrojuhelnik(v1, v2, v3);
                 }
             } else if (topologyType == TypGeometrickeTopologie.USECKA) {
                 // TODO
@@ -49,7 +49,7 @@ public class Renderer3D implements GPURenderer {
         }
     }
 
-    private void prepareTriangle(Vrchol v1, Vrchol v2, Vrchol v3) {
+    private void pripravTrojuhelnik(Vrchol v1, Vrchol v2, Vrchol v3) {
         // 1. transformace vrcholů
         Vrchol a = new Vrchol(
                 v1.getBod().mul(model).mul(view).mul(projection),
@@ -65,32 +65,39 @@ public class Renderer3D implements GPURenderer {
         );
 
         // 2. ořezání
-        // ořezat trojúhleníky, které jsou CELÉ mimo zobrazovací objem
+        // ořezat trojúhelníky, které jsou CELÉ mimo zobrazovací objem
         // slides 86 a 97
+
+        // podle podle osy x
         if (a.getX() > a.getW() && b.getX() > b.getW() && c.getX() > c.getW()) return; // trojúhelník je moc vpravo
         if (a.getX() < -a.getW() && b.getX() < -b.getW() && c.getX() < -c.getW()) return; // moc vlevo
-        // TODO podle y
+
+        // podle osy y
+        if (a.getY() > a.getW() && b.getY() > b.getW() && c.getY() > c.getW()) return; // moc nahoře
+        if (a.getY() < -a.getW() && b.getY() < -b.getW() && c.getY() < -c.getW()) return; // moc dole
+
+        // podle osy z
         if (a.getZ() > a.getW() && b.getZ() > b.getW() && c.getZ() > c.getW()) return; // moc vzadu/daleko (?)
         if (a.getZ() < 0 && b.getZ() < 0 && c.getZ() < 0) return; // je za námi
 
         // 3. seřazení podle Z
         // slide 101
+        // od javy 11 je možné využít var proměnnou, jen pro lokální proměnnou: var docasne = a;
         if (a.getZ() < b.getZ()) {
-            Vrchol temp = a;
+            Vrchol docasne = a;
             a = b;
-            b = temp;
+            b = docasne;
         }
         if (b.getZ() < c.getZ()) {
-//            var temp = b;
-            Vrchol temp = b;
+            Vrchol docasne = b;
             b = c;
-            c = temp;
+            c = docasne;
         }
         // teď je v C vrchol, který je nám nejblíže
         if (a.getZ() < b.getZ()) {
-            Vrchol temp = a;
+            Vrchol docasne = a;
             a = b;
-            b = temp;
+            b = docasne;
         }
 //        List<Vertex> collect = Stream.of(a, b, c).sorted(Comparator.comparingDouble(Vertex::getZ)).toList(); // ? not tested
 
@@ -98,30 +105,39 @@ public class Renderer3D implements GPURenderer {
 
         // 4. ořezání podle hrany Z
         // slide 101
+        // lineární interpolace
         if (a.getZ() < 0) {
             // A.Z je menší než nula = > všechny Z jsou menší než nula => není co zobrazit
         } else if (b.getZ() < 0) {
             // vrchol A je vidět, vrcholy B a C nejsou
+            // odečíst minimum: 0 - a.getZ() | dělit rozsahem: b.getZ() - a.getZ()
             double t1 = (0 - a.getZ()) / (b.getZ() - a.getZ());
             // 0 -> protože ten nový vrchol (ab), který má vzniknout, bude mít souřadnici Z nula
             Vrchol ab = a.mul(1 - t1).add(b.mul(t1));
 
-
             double t2 = -a.getZ() / (c.getZ() - a.getZ());
             Vrchol ac = a.mul(1 - t2).add(b.mul(t2));
 
-            drawTriangle(a, ab, ac);
+            nakresliTrojuhelnik(a, ab, ac);
 
         } else if (c.getZ() < 0) {
-            // TODO
-            // 2 krát se zavolá funkce drawTriangle()
+            // 2 krát se zavolá funkce nakresliTrojuhelnik()
+
+            double t1 = - a.getZ() / c.getZ() - a.getZ();
+            Vrchol ac = a.mul(1 - t1).add(c.mul(t1));
+
+            double t2 = -b.getZ() / c.getZ() - b.getZ();
+            Vrchol bc = b.mul(1 - t2).add(c.mul(t2));
+
+            nakresliTrojuhelnik(a, b, bc);
+            nakresliTrojuhelnik(a, ac, bc);
         } else {
             // vidíme celý trojúhelník
-            drawTriangle(a, b, c);
+            nakresliTrojuhelnik(a, b, c);
         }
     }
 
-    private void drawTriangle(Vrchol a, Vrchol b, Vrchol c) {
+    private void nakresliTrojuhelnik(Vrchol a, Vrchol b, Vrchol c) {
         // 1. dehomogenizace
         Optional<Vrchol> dA = a.dehomog();
         Optional<Vrchol> dB = b.dehomog();
@@ -135,13 +151,13 @@ public class Renderer3D implements GPURenderer {
         Vrchol v3 = dC.get();
 
         // 2. transformace do okna
-        Vec3D vec3D1 = transformToWindow(v1);
+        Vec3D vec3D1 = transformujDoOkna(v1);
         Vrchol aa = new Vrchol(new Point3D(vec3D1), v1.getBarva());
 
-        Vec3D vec3D2 = transformToWindow(v2);
+        Vec3D vec3D2 = transformujDoOkna(v2);
         Vrchol bb = new Vrchol(new Point3D(vec3D2), v2.getBarva());
 
-        Vec3D vec3D3 = transformToWindow(v3);
+        Vec3D vec3D3 = transformujDoOkna(v3);
         Vrchol cc = new Vrchol(new Point3D(vec3D3), v3.getBarva());
 
         // 3. seřazení podle Y
@@ -174,13 +190,13 @@ public class Renderer3D implements GPURenderer {
             double t2 = (y - aa.getY()) / (cc.getY() - aa.getY());
             Vrchol e = aa.mul(1 - t2).add(cc.mul(t2));
 
-            fillLine(d, e);
+            naplnUsecku(d, e);
         }
         // 2. for cyklus B->C
         // TODO
     }
 
-    private void fillLine(Vrchol a, Vrchol b) {
+    private void naplnUsecku(Vrchol a, Vrchol b) {
         if (a.getX() > b.getX()) {
             Vrchol temp = a;
             a = b;
@@ -194,11 +210,11 @@ public class Renderer3D implements GPURenderer {
             double t = (x - a.getX()) / (b.getX() - a.getX());
             Vrchol ab = a.mul(1 - t).add(b.mul(t));
 
-            drawPixel(x, (int) Math.round(ab.getY()), ab.getZ(), ab.getBarva());
+            nakresliPixel(x, (int) Math.round(ab.getY()), ab.getZ(), ab.getBarva());
         }
     }
 
-    private void drawPixel(int x, int y, double z, Col color) {
+    private void nakresliPixel(int x, int y, double z, Col color) {
         Optional<Double> zOptional = depthBuffer.getElement(x, y);
         if (zOptional.isPresent() && zOptional.get() > z) {
             depthBuffer.setElement(x, y, z);
@@ -206,13 +222,13 @@ public class Renderer3D implements GPURenderer {
         }
     }
 
-    private Vec3D transformToWindow(Vrchol vertex) {
+    private Vec3D transformujDoOkna(Vrchol vertex) {
         // přednáška PGI_F, slide 90
         return new Vec3D(vertex.getBod())
                 .mul(new Vec3D(1, -1, 1)) // Y jde nahoru a my chceme, aby šlo dolů
                 .add(new Vec3D(1, 1, 0)) // (0, 0) je uprostřed a my chceme, aby bylo vlevo nahoře
                 .mul(new Vec3D(imageBuffer.getWidth() / 2.0, imageBuffer.getHeight() / 2.0, 1));
-                // máme <0;2> -> vynásobíme polovinou velikosti plátna
+        // máme <0;2> -> vynásobíme polovinou velikosti plátna
     }
 
     @Override
